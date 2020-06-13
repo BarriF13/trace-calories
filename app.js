89adac81ff498e7d06721b63755dfc2150c45541
Barri <info@bdesignstudio.co.uk>
2020-06-13 21:24:01 +0100

    Set up controller

---
+++ app.js
@@ -0,0 +1,20 @@
+// Item Controller-UI Controller - App - Storage Controller
+
+
+r 
+
+// -1 Item Controller
+const ItemCtrl = (function(){
+  console.log('Item controler');
+})(); 
+
+//-2 UI Controller
+const UICtrl = (function(){
+  console.log('UI controler');
+})(); 
+
+//-3 App Controller
+
+const AppCtrl = (function(){
+  console.log('App controler');
+})(); 
